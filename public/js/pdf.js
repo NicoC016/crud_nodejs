9c6b55f77aa26a298606be14aa1525461a4be9e2
NicoC016 <nicoconil2018@gmail.com>
2022-02-04 18:17:37 -0300

    actualización y agregado de csv y pdf

---
+++ public/js/pdf.js
@@ -0,0 +1,54 @@
+
+// $(document).ready(function(){
+//   function exportTable(){
+//     TableExport(document.getElementById("#tableProduct"),{
+//       headers: true,                      // (Boolean), display table headers (th or td elements) in the <thead>, (default: true)
+//       footers: false,                      // (Boolean), display table footers (th or td elements) in the <tfoot>, (default: false)
+//       formats: ["pdf", "csv"],             // (String[]), filetype(s) for the export, (default: ['xlsx', 'csv', 'txt'])
+//       filename: "lista",                     // (id, String), filename for the downloaded file, (default: 'id')
+//       bootstrap: true,                   // (Boolean), style buttons using bootstrap, (default: true)
+//       exportButtons: true,                // (Boolean), automatically generate the built-in export buttons for each of the specified formats (default: true)
+//       position: "top",                 // (top, bottom), position of the caption element relative to table, (default: 'bottom')
+//       ignoreRows: null,                   // (Number, Number[]), row indices to exclude from the exported file(s) (default: null)
+//       ignoreCols: null,                   // (Number, Number[]), column indices to exclude from the exported file(s) (default: null)
+//       trimWhitespace: true,               // (Boolean), remove all leading/trailing newlines, spaces, and tabs from cell text in the exported file(s) (default: false)
+//       RTL: false,                         // (Boolean), set direction of the worksheet to right-to-left (default: false)
+//       sheetname: "table"                     // (id, String), sheet name for the exported spreadsheet, (default: 'id')
+
+//     });
+//   }
+// })  
+
+
+// function Pdf() {
+//   var pdf = new jsPDF ({
+//     unit: "px",
+//     format: "a4",
+//   });
+//   source = $('#tableProduct')[0]
+
+//   specialElementHandlers = {
+//     '#bypassme': function (element, renderer) {
+//       return true
+//     }
+//   };
+//   margins = {
+//     top: 8,
+//     bottom: 60,
+//     left: 100,
+//     width: 522
+//   };
+
+//   pdf.fromHTML(
+//     source, 
+//     margins.left, // x coord
+//     margins.top, { // y coord
+//       'width': margins.width, 
+//       'elementHandlers': specialElementHandlers
+//     },
+
+//     function (dispose) {
+//         pdf.save('lista_producto.pdf');
+//      }, margins
+//   );
+// }
